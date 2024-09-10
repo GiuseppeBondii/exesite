901e9beb99be85d8f366551acf2952d0585069b7
GiuseppeBondii <131357828+GiuseppeBondii@users.noreply.github.com>
2024-09-10 17:36:48 +0200

    Aggiornamento cavolate

--- src/esami/HomeEsami.js
+++ src/esami/HomeEsami.js
@@ -11,20 +11,18 @@ function HomeEsami(){
         <ul>
             <div className='casa'>
           <li><Link to="../">Home</Link></li></div>
-          <li><Link to="../esami/ESAME1">ESAME1</Link></li>
-          <li><Link to="../esami/ESAME2">ESAME2</Link></li>
-          <li><Link to="../esami/ESAME3">ESAME3</Link></li>
-          <li><Link to="../esami/ESAME4">ESAME4(si potrebbe incliudere nel progetto)</Link></li>
-          <li><Link to="../esami/ESAME5">ESAME5</Link></li>
-          <li><Link to="../esami/ESAME6">ESAME6</Link></li>
-          <li><Link to="../esami/ESAME7">ESAME7</Link></li>
-          <li><Link to="../esami/ESAME8/ESAME8">ESAME8</Link></li>
-          <li><Link to="../esami/ESAME9">ESAME9</Link></li>
-          <li><Link to="../esami/ESAME10">ESAME10</Link></li>
-          <li><Link to="../esami/ESAME11">ESAME11</Link></li>
-          <li><Link to="../esami/ESAME12">ESAME12NON APRIRE</Link></li>
-
-
+          <li><Link to="../esami/ESAME1">Responsive e uso di @media</Link></li>
+          <li><Link to="../esami/ESAME2">Appuntamento Medico</Link></li>
+          <li><Link to="../esami/ESAME3">Responsive e uso di @media</Link></li>
+          <li><Link to="../esami/ESAME4">getore di appuntamenti(si potrebbe incliudere nel progetto)</Link></li>
+          <li><Link to="../esami/ESAME5">Responsive e uso di @media</Link></li>
+          <li><Link to="../esami/ESAME6">Navbar e Dropdown senza Bootstrap</Link></li>
+          <li><Link to="../esami/ESAME7">API Attori</Link></li>
+          <li><Link to="../esami/ESAME8/ESAME8">Responsive Pintrest</Link></li>
+          <li><Link to="../esami/ESAME9">To Do List(si potrebbe incliudere nel progetto)</Link></li>
+          <li><Link to="../esami/ESAME10">Galleria Immagini</Link></li>
+          <li><Link to="../esami/ESAME11">Behance Form Accesso</Link></li>
+          <li><Link to="../esami/ESAME12">NON APRIRE (API OpenTranslate)</Link></li>
 
         </ul>
       </nav>
